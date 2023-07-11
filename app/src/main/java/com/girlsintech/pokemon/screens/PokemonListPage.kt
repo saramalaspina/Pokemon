f0beaeee8c3016fb26adcb4e06a032cb6184b8d3
costanzagiacona <costanza.giacona@libero.it>
2023-07-11 18:42:15 +0200

    pokemon list page

--- app/src/main/java/com/girlsintech/pokemon/screens/PokemonListPage.kt
+++ app/src/main/java/com/girlsintech/pokemon/screens/PokemonListPage.kt
@@ -2,8 +2,10 @@ package com.girlsintech.pokemon.screens
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -19,10 +21,16 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.girlsintech.pokemon.R
 import androidx.compose.material.Text
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.platform.LocalContext
+import coil.request.ImageRequest
+import com.girlsintech.pokemon.data.models.PokemonItem
+import com.girlsintech.pokemon.viewmodel.PokemonListViewModel
 
 
 @Composable
-fun PokemonListPage (
+fun PokemonListPage(
     navController: NavController
 ) {
     Surface(
@@ -91,3 +99,51 @@ fun SearchBar(
         }
     }
 }
+/*
+@Composable
+fun PokemonEntry(
+    entry: PokemonItem,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    viewModel: PokemonListViewModel = hiltNavGraphViewModel()
+) {
+    val defaultDominantColor = MaterialTheme.colors.surface
+    var dominantColor = remember {
+        mutableStateOf(defaultDominantColor)
+    }
+
+    Box(
+        contentAlignment = Center,
+        modifier = modifier
+            .shadow(5.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .aspectRatio(1f)
+            .background(
+                Brush.verticalGradient(
+                    listOf(
+                        dominantColor,
+                        defaultDominantColor
+                    )
+                )
+            )
+            .clickable {
+                navController.navigate(
+                    "pokemon_detail_screen/${dominantColor.toArgb()}/${entry.pokemonName}"
+                ) {
+                    Column {
+                        CoilImage(
+                            request = ImageRequest.Builder(LocalContext.current)
+                                .data(entry.imageUrl)
+                                .target{
+                                    viewModel.calcDominantColor(it){ color ->
+
+                                    }
+                                }
+                        )
+                    }
+                }
+            }
+    )
+}
+
+ */
