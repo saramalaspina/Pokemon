59cb27d20aa0b1d2e4e7681669788cbfe49cda16
saramalaspina <saramalaspina2001@gmail.com>
2023-08-22 15:33:04 +0200

    Constants.kt

--- app/src/main/java/com/girlsintech/pokemon/util/Constants.kt
+++ app/src/main/java/com/girlsintech/pokemon/util/Constants.kt
@@ -1,13 +1,5 @@
 package com.girlsintech.pokemon.util
 
 object Constants {
-    const val BASE_URL = "https://pokeapi.co/api/v2/pokemon"
-
     const val IMAGE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
-
-    const val SPECIES_URL = "https://pokeapi.co/api/v2/pokemon-species/"
-
-    const val EVOLUTION_URL = "https://pokeapi.co/api/v2/evolution-chain/"
-
-    const val PAGE_SIZE = 3
 }
